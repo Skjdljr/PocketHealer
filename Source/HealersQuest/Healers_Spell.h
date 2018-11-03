9cd6286e81608a9c3bb3cfb835579e782482e2d5
Joel <disturbed_4u2001@yahoo.com>
2018-11-03 00:24:59 -0400

    Added the spell icon
    Updated spells with some info and icons however need an actual instance of the spell

--- Source/HealersQuest/Healers_Spell.h
+++ Source/HealersQuest/Healers_Spell.h
@@ -19,20 +19,23 @@ class HEALERSQUEST_API AHealers_Spell : public AActor
     GENERATED_BODY()
 
 public:
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers Spell")
     TArray<UHealers_SpellComponent*> SpellComponents;
 
     /* Component that's responsible for specifying which actors this spell will affect. */
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers Spell")
     UHealers_SpellTargetingComponent* TargetComponent;
 
     /* List of components that determines if this spell can be casted on target or not. */
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers Spell")
     TArray<UHealers_SpellPrerequisiteComponent*> Prerequisites;
 
     /* Human readable name of this spell (meant to be displayed on UI for example). */
-    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category="Healers")
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category="Healers Spell")
     FText SpellName;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Healers Spell")
+    UTexture2D* SpellIcon;
 
     AHealers_Spell(const FObjectInitializer& ObjectInitializer);
     virtual void BeginPlay () override;
@@ -41,15 +44,15 @@ public:
 
     /* Returns true if all requirements are met, and the user is allowed to choose this spell.
     Useful for conditions before a target is selected (eg:  is it on cooldown?  Do you have enough mana?) */
-    UFUNCTION(BlueprintCallable, Category="Healers")
+    UFUNCTION(BlueprintCallable, Category="Healers Spell")
     virtual bool CanCastSpell(APawn* caster) const;
 
     /* Returns true if all requirements are met, and the user is allowed to cast the spell on specified actors.
     This is useful if you want to deny the spell cast because the user has chosen an invalid target. */
-    UFUNCTION(BlueprintCallable, Category="Healers")
+    UFUNCTION(BlueprintCallable, Category="Healers Spell")
     virtual bool CanCastSpellOnTargets(APawn* caster, const TArray<AActor*>& targets) const;
 
-    UFUNCTION(BlueprintCallable, Category="Healers")
+    UFUNCTION(BlueprintCallable, Category="Healers Spell")
     /* Initializes the spell components and executes the spell sequence. */
     virtual void CastSpell(APawn* caster, const TArray<AActor*>& targets);
 };
