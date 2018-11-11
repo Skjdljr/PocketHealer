845f789854230104da50837357ecf09c83204c8f
wail <wailofsuicide@gmail.com>
2018-11-11 10:27:52 -0500

    PartySheet - Deprecating PlayerCharacterSheet

--- Source/HealersQuest/Healers_PartySheet.h
+++ Source/HealersQuest/Healers_PartySheet.h
@@ -19,12 +19,12 @@ class AHealers_CharacterSheet;
  * Store Information about our Party
  */
 UCLASS(BlueprintType, Blueprintable)
-class HEALERSQUEST_API AHealers_PartySheet : public AActor //AInfo
+class HEALERSQUEST_API AHealers_PartySheet : public AInfo
 {
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Healers|PartySheet")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (Deprecated), Category = "Healers|PartySheet")
 	AHealers_CharacterSheet* PlayerCharacterSheet;
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Healers|PartySheet")
