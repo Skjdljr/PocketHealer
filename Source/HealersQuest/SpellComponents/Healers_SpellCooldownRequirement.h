1fb9ee5ff012c4f0b24e6a7511754548e1131149
wail <wailofsuicide@gmail.com>
2018-11-12 01:24:45 -0500

    Cleanup / Untabify

--- Source/HealersQuest/SpellComponents/Healers_SpellCooldownRequirement.h
+++ Source/HealersQuest/SpellComponents/Healers_SpellCooldownRequirement.h
@@ -15,22 +15,22 @@ class HEALERSQUEST_API UHealers_SpellCooldownRequirement : public UHealers_Spell
     GENERATED_BODY()
 
 public:
-	/* Number of seconds that must elapse before this spell can be casted again. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
-	float SpellCastInterval;
+    /* Number of seconds that must elapse before this spell can be casted again. */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
+    float SpellCastInterval;
 
-	/* Number of seconds remaining before this spell becomes available again.
-	If negative, then this spell is ready. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
-	float Cooldown;
+    /* Number of seconds remaining before this spell becomes available again.
+    If negative, then this spell is ready. */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
+    float Cooldown;
 
-	/* Multiplier in how fast the cooldown depletes (think alacrity) */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
-	float CooldownMultiplier;
+    /* Multiplier in how fast the cooldown depletes (think alacrity) */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Healers")
+    float CooldownMultiplier;
 
     UHealers_SpellCooldownRequirement(const FObjectInitializer& ObjectInitializer);
 
-	virtual bool CanCastSpell(AHealers_CharacterSheet* caster) const override;
-	virtual void SpellExecuted(AHealers_CharacterSheet* caster) override;
-	virtual void TickPrerequisite(float deltaSec) override;
+    virtual bool CanCastSpell(AHealers_CharacterSheet* caster) const override;
+    virtual void SpellExecuted(AHealers_CharacterSheet* caster) override;
+    virtual void TickPrerequisite(float deltaSec) override;
 };
