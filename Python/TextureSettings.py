dd27ae02a1cf1d9469e173ed13b536d89b95f16b
wail <wailofsuicide@gmail.com>
2018-12-16 19:47:10 -0500

    Update script to ensure it only acts on Texture2D class

--- Python/TextureSettings.py
+++ Python/TextureSettings.py
@@ -1,3 +1,7 @@
+# TextureSettings.py
+# 
+# Reference: https://api.unrealengine.com/INT/PythonAPI/
+
 import unreal
 
 aregistry = None
@@ -14,42 +18,36 @@ assetlist = aregistry.get_assets_by_path("/Game/Sprites/UI/Icons", True, True)
 #print len(uitextures)
 
 for textureasset in assetlist:
-    #print textureasset
-    texture = textureasset.get_asset()
-    
-    #print texture.lod_group
-    print texture.compression_settings
-    
-    # These are equivalent
-    #print texture.mip_gen_settings
-    #print texture.get_editor_property("mip_gen_settings")
-    
-    
-    #if texture.never_stream == False:
-    #    texture.never_stream = True;
-    #    print "We did it"
-    
-    # Set our LOD Group to  TEXTUREGROUP_UI(15)
-    texturelodgroup = unreal.TextureGroup.cast(16)
-    if texture.get_editor_property("lod_group") != texturelodgroup:
-        texture.set_editor_property("lod_group", texturelodgroup)
-    
-    # Set our CompressionSetting to UserInterface2D (7)
-    texturecompressionsetting = unreal.TextureCompressionSettings.cast(7)
-    if texture.get_editor_property("compression_settings") != texturecompressionsetting:
-        texture.set_editor_property("compression_settings", texturecompressionsetting)
-        count = count + 1
-    
-    # Set our MipGenSetting to NoMipMaps (13)
-    texturemipsetting = unreal.TextureMipGenSettings.cast(13)
-    if texture.get_editor_property("mip_gen_settings") != texturemipsetting:
-        texture.set_editor_property("mip_gen_settings", texturemipsetting)
-    
-    # Set our NeverStream property to True
-    if texture.get_editor_property("never_stream") == False:
-        texture.set_editor_property("never_stream", True)
-    
-    # Save the asset (True == only_if_is_dirty)
-    unreal.EditorAssetLibrary.save_asset(textureasset.get_full_name(), True)
+    # Our TextureAsset is an AssetData object. We want a Texture2D object
+    # Ensure it's a Texture2D
+    if unreal.Texture2D.static_class() == texture.get_class():
+        texture = textureasset.get_asset()
+        
+        # These are equivalent
+        #print texture.mip_gen_settings
+        #print texture.get_editor_property("mip_gen_settings")
+        
+        # Set our LOD Group to  TEXTUREGROUP_UI(15)
+        texturelodgroup = unreal.TextureGroup.cast(16)
+        if texture.get_editor_property("lod_group") != texturelodgroup:
+            texture.set_editor_property("lod_group", texturelodgroup)
+        
+        # Set our CompressionSetting to UserInterface2D (7)
+        texturecompressionsetting = unreal.TextureCompressionSettings.cast(7)
+        if texture.get_editor_property("compression_settings") != texturecompressionsetting:
+            texture.set_editor_property("compression_settings", texturecompressionsetting)
+            count = count + 1
+        
+        # Set our MipGenSetting to NoMipMaps (13)
+        texturemipsetting = unreal.TextureMipGenSettings.cast(13)
+        if texture.get_editor_property("mip_gen_settings") != texturemipsetting:
+            texture.set_editor_property("mip_gen_settings", texturemipsetting)
+        
+        # Set our NeverStream property to True
+        if texture.get_editor_property("never_stream") == False:
+            texture.set_editor_property("never_stream", True)
+        
+        # Save the asset (path, True == only_if_is_dirty)
+        unreal.EditorAssetLibrary.save_asset(textureasset.get_full_name(), True)
 
 #end For-loop
