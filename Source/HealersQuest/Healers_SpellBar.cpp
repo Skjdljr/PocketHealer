dbcf6a15055ba13c78f99a007d598fb7f19c5533
wail <wailofsuicide@gmail.com>
2019-03-29 21:31:46 -0400

    Initial SpellBar Actor Class
    - Holds Spells, intended to intermediate PC Keybinds, CharacterSheet Spells, and be persistent beyond Combat

---
+++ Source/HealersQuest/Healers_SpellBar.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Healers_SpellBar.h"
+
+#include "Healers_Spell.h"
+#include "Healers_CharacterSheet.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+AHealers_SpellBar::AHealers_SpellBar()
+    : Caster(nullptr)
+    , SelectedSpell(nullptr)
+{
+}
+
+bool AHealers_SpellBar::SelectSpell(int32 Index)
+{
+    if (SpellBar.Num() > Index)
+    {
+        AHealers_Spell* CurrentSpell = SpellBar[Index];
+
+        if (CurrentSpell->CanCastSpell(Caster))
+        {
+            // TODO : If TargetComponent == SingleTarget || MultiTargetComponent
+            if (CurrentSpell->TargetComponent)
+            {
+
+            }
+            // TODO else if AutoTarget, SelfTarget, GlobalTarget... Try to Cast
+            else
+            {
+                SelectedSpell = CurrentSpell;
+
+                const TArray<AHealers_CharacterSheet*> Targets = TArray<AHealers_CharacterSheet*>();
+                SelectedSpell->CastSpell(nullptr, Targets);
+            }
+        }        
+    }
+
+    return SelectedSpell;
+}
---
+++ Source/HealersQuest/Healers_SpellBar.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Healers_SpellBar.generated.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Forward Declaration
+
+class AHealers_Spell;
+class AHealers_CharacterSheet;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * 
+ */
+UCLASS()
+class HEALERSQUEST_API AHealers_SpellBar : public AActor
+{
+    GENERATED_BODY()
+
+    AHealers_SpellBar();
+
+public:
+    
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Healers|SpellBar")
+    AHealers_CharacterSheet* Caster;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Healers|SpellBar")
+    AHealers_Spell* SelectedSpell;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Healers|SpellBar")
+    TArray<AHealers_Spell*> SpellBar;
+
+    UFUNCTION(BlueprintCallable, Category = "Healers|SpellBar")
+    bool SelectSpell(int32 Index);
+};
