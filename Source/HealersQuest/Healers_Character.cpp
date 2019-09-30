6154140a6b821c8a4d59569bdfc99a2552a76ae2
wail <wailofsuicide@gmail.com>
2019-09-29 23:16:54 -0400

    Adding AbilityComponent to Character

--- Source/HealersQuest/Healers_Character.cpp
+++ Source/HealersQuest/Healers_Character.cpp
@@ -12,6 +12,7 @@ AHealers_Character::AHealers_Character()
     // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 
+    AbilityComponent = CreateDefaultSubobject<UHealers_AbilitySystemComponent>("AbilityComponent");
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
--- Source/HealersQuest/Healers_Character.h
+++ Source/HealersQuest/Healers_Character.h
@@ -6,6 +6,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "Healers_AbilitySystemComponent.h"
+
 #include "Healers_Character.generated.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -13,21 +16,26 @@
 UCLASS()
 class HEALERSQUEST_API AHealers_Character : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AHealers_Character();
+    // Sets default values for this character's properties
+    AHealers_Character();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+    /*
+    * Ability System Component
+    */
+    UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Character")
+    UHealers_AbilitySystemComponent* AbilityComponent;
 };
