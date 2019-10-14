09d86a980e33e904e59d4360adbafe2fb43d8661
wail <wailofsuicide@gmail.com>
2019-10-14 17:51:07 -0400

    Updating Character with code from ActionRPG

--- Source/HealersQuest/Healers_Character.cpp
+++ Source/HealersQuest/Healers_Character.cpp
@@ -5,6 +5,11 @@
 #include "Healers_Character.h"
 
 #include "UnrealNetwork.h"
+#include "Abilities/GameplayAbilityTypes.h"
+
+#include "Healers_AbilitySystemComponent.h"
+#include "Healers_AttributeSet.h"
+#include "Healers_GameplayAbility.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -14,7 +19,11 @@ AHealers_Character::AHealers_Character()
     // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 
-    AbilityComponent = CreateDefaultSubobject<UHealers_AbilitySystemComponent>("AbilityComponent");
+
+    AbilitySystemComponent = CreateDefaultSubobject<UHealers_AbilitySystemComponent>("AbilitySystemComponent");
+
+    // Create the attribute set, this replicates by default
+    AttributeSet = CreateDefaultSubobject<UHealers_AttributeSet>(TEXT("AttributeSet"));
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -23,7 +32,7 @@ void AHealers_Character::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& O
 {
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
-    DOREPLIFETIME(AHealers_Character, AbilityComponent);
+    DOREPLIFETIME(AHealers_Character, CharacterLevel);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -49,3 +58,188 @@ void AHealers_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 
 }
 
+UAbilitySystemComponent* AHealers_Character::GetAbilitySystemComponent() const
+{
+    return AbilitySystemComponent;
+}
+
+int32 AHealers_Character::GetCharacterLevel() const
+{
+    return CharacterLevel;
+}
+
+bool AHealers_Character::SetCharacterLevel(int32 NewLevel)
+{
+    bool bSuccessful{ false };
+
+    if (CharacterLevel != NewLevel && NewLevel > 0)
+    {
+        // Our level changed so we need to refresh abilities
+        RemoveStartupGameplayAbilities();
+        CharacterLevel = NewLevel;
+        AddStartupGameplayAbilities();
+
+        bSuccessful = true;
+    }
+
+    return bSuccessful;
+}
+
+void AHealers_Character::AddStartupGameplayAbilities()
+{
+    check(AbilitySystemComponent);
+
+    if (Role == ROLE_Authority && !bAbilitiesInitialized)
+    {
+        // Grant abilities, but only on the server	
+        for (TSubclassOf<UHealers_GameplayAbility>& StartupAbility : GameplayAbilities)
+        {
+            AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(StartupAbility, GetCharacterLevel(), INDEX_NONE, this));
+        }
+
+        // Now apply passives
+        for (TSubclassOf<UGameplayEffect>& GameplayEffect : PassiveGameplayEffects)
+        {
+            FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+            EffectContext.AddSourceObject(this);
+
+            FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffect, GetCharacterLevel(), EffectContext);
+            if (NewHandle.IsValid())
+            {
+                FActiveGameplayEffectHandle ActiveGEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*NewHandle.Data.Get(), AbilitySystemComponent);
+            }
+        }
+
+        AddSlottedGameplayAbilities();
+
+        bAbilitiesInitialized = true;
+    }
+}
+
+void AHealers_Character::RemoveStartupGameplayAbilities()
+{
+    check(AbilitySystemComponent);
+
+    if (Role == ROLE_Authority && bAbilitiesInitialized)
+    {
+        // Remove any abilities added from a previous call
+        TArray<FGameplayAbilitySpecHandle> AbilitiesToRemove;
+        for (const FGameplayAbilitySpec& Spec : AbilitySystemComponent->GetActivatableAbilities())
+        {
+            if ((Spec.SourceObject == this) && GameplayAbilities.Contains(Spec.Ability->GetClass()))
+            {
+                AbilitiesToRemove.Add(Spec.Handle);
+            }
+        }
+
+        // Do in two passes so the removal happens after we have the full list
+        for (int32 i = 0; i < AbilitiesToRemove.Num(); i++)
+        {
+            AbilitySystemComponent->ClearAbility(AbilitiesToRemove[i]);
+        }
+
+        // Remove all of the passive gameplay effects that were applied by this character
+        FGameplayEffectQuery Query;
+        Query.EffectSource = this;
+        AbilitySystemComponent->RemoveActiveEffects(Query);
+
+        RemoveSlottedGameplayAbilities(true);
+
+        bAbilitiesInitialized = false;
+    }
+}
+
+void AHealers_Character::AddSlottedGameplayAbilities()
+{
+    TMap<FRPGItemSlot, FGameplayAbilitySpec> SlottedAbilitySpecs;
+    FillSlottedAbilitySpecs(SlottedAbilitySpecs);
+
+    // Now add abilities if needed
+    for (const TPair<FRPGItemSlot, FGameplayAbilitySpec>& SpecPair : SlottedAbilitySpecs)
+    {
+        FGameplayAbilitySpecHandle& SpecHandle = SlottedAbilities.FindOrAdd(SpecPair.Key);
+
+        if (!SpecHandle.IsValid())
+        {
+            SpecHandle = AbilitySystemComponent->GiveAbility(SpecPair.Value);
+        }
+    }
+}
+
+void AHealers_Character::RemoveSlottedGameplayAbilities(bool bRemoveAll)
+{
+    TMap<FRPGItemSlot, FGameplayAbilitySpec> SlottedAbilitySpecs;
+
+    if (!bRemoveAll)
+    {
+        // Fill in map so we can compare
+        FillSlottedAbilitySpecs(SlottedAbilitySpecs);
+    }
+
+    for (TPair<FRPGItemSlot, FGameplayAbilitySpecHandle>& ExistingPair : SlottedAbilities)
+    {
+        FGameplayAbilitySpec* FoundSpec = AbilitySystemComponent->FindAbilitySpecFromHandle(ExistingPair.Value);
+        bool bShouldRemove = bRemoveAll || !FoundSpec;
+
+        if (!bShouldRemove)
+        {
+            // Need to check desired ability specs, if we got here FoundSpec is valid
+            FGameplayAbilitySpec* DesiredSpec = SlottedAbilitySpecs.Find(ExistingPair.Key);
+
+            if (!DesiredSpec || DesiredSpec->Ability != FoundSpec->Ability || DesiredSpec->SourceObject != FoundSpec->SourceObject)
+            {
+                bShouldRemove = true;
+            }
+        }
+
+        if (bShouldRemove)
+        {	
+            if (FoundSpec)
+            {
+                // Need to remove registered ability
+                AbilitySystemComponent->ClearAbility(ExistingPair.Value);
+            }
+
+            // Make sure handle is cleared even if ability wasn't found
+            ExistingPair.Value = FGameplayAbilitySpecHandle();
+        }
+    }
+}
+
+void AHealers_Character::FillSlottedAbilitySpecs(TMap<FRPGItemSlot, FGameplayAbilitySpec>& SlottedAbilitySpecs)
+{
+    // First add default ones
+    for (const TPair<FRPGItemSlot, TSubclassOf<UHealers_GameplayAbility>>& DefaultPair : DefaultSlottedAbilities)
+    {
+        if (DefaultPair.Value.Get())
+        {
+            SlottedAbilitySpecs.Add(DefaultPair.Key, FGameplayAbilitySpec(DefaultPair.Value, GetCharacterLevel(), INDEX_NONE, this));
+        }
+    }
+
+    // Now potentially override with inventory
+    //if (InventorySource)
+    //{
+    //    const TMap<FRPGItemSlot, URPGItem*>& SlottedItemMap = InventorySource->GetSlottedItemMap();
+
+    //    for (const TPair<FRPGItemSlot, URPGItem*>& ItemPair : SlottedItemMap)
+    //    {
+    //        URPGItem* SlottedItem = ItemPair.Value;
+
+    //        // Use the character level as default
+    //        int32 AbilityLevel = GetCharacterLevel();
+
+    //        if (SlottedItem && SlottedItem->ItemType.GetName() == FName(TEXT("Weapon")))
+    //        {
+    //            // Override the ability level to use the data from the slotted item
+    //            AbilityLevel = SlottedItem->AbilityLevel;
+    //        }
+
+    //        if (SlottedItem && SlottedItem->GrantedAbility)
+    //        {
+    //            // This will override anything from default
+    //            SlottedAbilitySpecs.Add(ItemPair.Key, FGameplayAbilitySpec(SlottedItem->GrantedAbility, AbilityLevel, INDEX_NONE, SlottedItem));
+    //        }
+    //    }
+    //}
+}
--- Source/HealersQuest/Healers_Character.h
+++ Source/HealersQuest/Healers_Character.h
@@ -6,15 +6,74 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-
-#include "Healers_AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
 
 #include "Healers_Character.generated.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+class UHealers_AbilitySystemComponent;
+class UHealers_AttributeSet;
+class UHealers_GameplayAbility;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/** Struct representing a slot for an item, shown in the UI */
+USTRUCT(BlueprintType)
+struct HEALERSQUEST_API FRPGItemSlot
+{
+    GENERATED_BODY()
+
+        /** Constructor, -1 means an invalid slot */
+        FRPGItemSlot()
+        : SlotNumber(-1)
+    {}
+
+    FRPGItemSlot(const FPrimaryAssetType& InItemType, int32 InSlotNumber)
+        : ItemType(InItemType)
+        , SlotNumber(InSlotNumber)
+    {}
+
+    /** The type of items that can go in this slot */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
+        FPrimaryAssetType ItemType;
+
+    /** The number of this slot, 0 indexed */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
+        int32 SlotNumber;
+
+    /** Equality operators */
+    bool operator==(const FRPGItemSlot& Other) const
+    {
+        return ItemType == Other.ItemType && SlotNumber == Other.SlotNumber;
+    }
+    bool operator!=(const FRPGItemSlot& Other) const
+    {
+        return !(*this == Other);
+    }
+
+    /** Implemented so it can be used in Maps/Sets */
+    friend inline uint32 GetTypeHash(const FRPGItemSlot& Key)
+    {
+        uint32 Hash = 0;
+
+        Hash = HashCombine(Hash, GetTypeHash(Key.ItemType));
+        Hash = HashCombine(Hash, (uint32)Key.SlotNumber);
+        return Hash;
+    }
+
+    /** Returns true if slot is valid */
+    bool IsValid() const
+    {
+        return ItemType.IsValid() && SlotNumber >= 0;
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 UCLASS()
-class HEALERSQUEST_API AHealers_Character : public ACharacter
+class HEALERSQUEST_API AHealers_Character : public ACharacter,
+    public IAbilitySystemInterface
 {
     GENERATED_BODY()
 
@@ -33,9 +92,72 @@ public:
     // Called to bind functionality to input
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+    // Implement IAbilitySystemInterface
+    UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+    /** Returns the character level that is passed to the ability system */
+    UFUNCTION(BlueprintCallable)
+    virtual int32 GetCharacterLevel() const;
+
+    /** Modifies the character level, this may change abilities. Returns true on success */
+    UFUNCTION(BlueprintCallable)
+    virtual bool SetCharacterLevel(int32 NewLevel);
+
+    /**
+    * Apply the startup gameplay abilities and effects
+    */
+    virtual void AddStartupGameplayAbilities();
+
+    /** Attempts to remove any startup gameplay abilities */
+    void RemoveStartupGameplayAbilities();
+
+    /** Adds slotted item abilities if needed */
+    void AddSlottedGameplayAbilities();
+
+    /** Remove slotted gameplay abilities, if force is false it only removes invalid ones */
+    void RemoveSlottedGameplayAbilities(bool bRemoveAll);
+
+    /** Fills in with ability specs, based on defaults and inventory */
+    void FillSlottedAbilitySpecs(TMap<FRPGItemSlot, FGameplayAbilitySpec>& SlottedAbilitySpecs);
+
+
+protected:
+
+
     /*
     * Ability System Component
     */
-    UPROPERTY(Replicated, BlueprintReadOnly, VisibleAnywhere, Category = "Character")
-    UHealers_AbilitySystemComponent* AbilityComponent;
+    UPROPERTY()
+    UHealers_AbilitySystemComponent* AbilitySystemComponent;
+
+    /**
+    * Attribute Set
+    */
+    UPROPERTY()
+    UHealers_AttributeSet* AttributeSet;
+
+    UPROPERTY()
+    int32 bAbilitiesInitialized;
+
+    /** The level of this character, should not be modified directly once it has already spawned */
+    UPROPERTY(Replicated, EditAnywhere, Category = "Abilities")
+    int32 CharacterLevel;
+
+    /** Abilities to grant to this character on creation. These will be activated by tag or event and are not bound to specific inputs */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Abilities")
+    TArray<TSubclassOf<UHealers_GameplayAbility>> GameplayAbilities;
+
+    /** Map of item slot to gameplay ability class, these are bound before any abilities added by the inventory */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Abilities)
+    TMap<FRPGItemSlot, TSubclassOf<UHealers_GameplayAbility>> DefaultSlottedAbilities;
+
+    /** Passive gameplay effects applied on creation */
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
+    TArray<TSubclassOf<UGameplayEffect>> PassiveGameplayEffects;
+
+    /** Map of slot to ability granted by that slot. I may refactor this later */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Inventory)
+    TMap<FRPGItemSlot, FGameplayAbilitySpecHandle> SlottedAbilities;
 };
