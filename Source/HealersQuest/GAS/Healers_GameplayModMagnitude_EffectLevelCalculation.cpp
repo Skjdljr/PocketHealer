33dc4fe186646818f37821e6c52dfd13c84d1fe7
wail <wailofsuicide@gmail.com>
2019-11-23 23:25:49 -0500

    Updating Skill Selection Screen
    - Players now have Skill Points. When you purchase a skill, it decrements the skillpoints value
    - Skillpoints count value refreshes when you spend them
    - Skill Purchase buttons disable when you have zero skillpoints
    - Refund Skill button will refund all your skillpoints for a given skill

---
+++ Source/HealersQuest/GAS/Healers_GameplayModMagnitude_EffectLevelCalculation.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Healers_GameplayModMagnitude_EffectLevelCalculation.h"
+
+#include "HealersQuest.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+float UHealers_GameplayModMagnitude_EffectLevelCalculation::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+    GAME_LOG(Game, Log, "GameplayEffect Level: %d", Spec.GetLevel());
+    return Spec.GetLevel();
+}
---
+++ Source/HealersQuest/GAS/Healers_GameplayModMagnitude_EffectLevelCalculation.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "Healers_GameplayModMagnitude_EffectLevelCalculation.generated.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * 
+ */
+UCLASS()
+class HEALERSQUEST_API UHealers_GameplayModMagnitude_EffectLevelCalculation : public UGameplayModMagnitudeCalculation
+{
+    GENERATED_BODY()
+    
+public:
+
+    float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const;
+};
