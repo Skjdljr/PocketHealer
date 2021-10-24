6d409bd6c5ee86807ce785963055a1538fd44efb
wail <wailofsuicide@gmail.com>
2021-10-24 14:34:16 -0400

    Upgrade Project to UE 4.27
    - Rename Project in Launcher
    - Fix missing UMG Dependency in Build.cs

--- Source/HealersQuest/HealersQuest.Build.cs
+++ Source/HealersQuest/HealersQuest.Build.cs
@@ -8,7 +8,7 @@ public class HealersQuest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities", "GameplayTags", "GameplayTasks", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
